058f0378c41f89172a5c39c4c8623b5c05d8495c
gustavo <gustavo.geisleschter@gmail.com>
2026-02-15 15:43:43 -0300

    geral

--- aulas/recebendo_dados_usuario.py
+++ aulas/recebendo_dados_usuario.py
@@ -16,5 +16,5 @@ idade = input()
 # print('Seja bem vindo(a) %s', % nome), versão 2.x
 # print('Seja bem vindo(a) {0}', .format(nome)), versão 3.x
 print(f'Seja bem vindo(a) {nome} \nSua idade é: {idade}')  # Versão atual
-#nome = input('Qual seu nome?') versão simplificada
-#idade = input('Qual sua idade?') versão simplificada
+# nome = input('Qual seu nome?') versão simplificada
+# idade = input('Qual sua idade?') versão simplificada

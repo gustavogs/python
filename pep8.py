f4de593b8ab78c1410f1394f91a6672668c9cb2c
gustavo <gustavo.geisleschter@gmail.com>
2026-02-15 14:35:47 -0300

    geral

--- pep8.py
+++ pep8.py
@@ -1,1 +1,52 @@
 import this
+
+"""
+PEP8 - Python Enhanced Proposals
+
+São propostas de melhorias gerais para a linguagem
+
+The Zen of Python:
+
+import this
+"""
+
+"""
+A ideia da PEP8 é que escrevamos codigos PYTHONICOS.
+
+[1] - Utilize "Camel case" para nomes de clases.
+
+[2] - Utilize nomes em minusculo, separados em underline para funções
+ou variaveis.
+
+[3] - Utilize quatro espaços para identação.
+"""
+
+"[1]"
+
+
+class Calculadora:
+    pass
+
+
+class CalculadoraCientifica:
+    pass
+
+
+"[2]"
+
+
+def soma():
+    pass
+
+
+def soma_dois():
+    pass
+
+
+numero = 2
+numero_impar = 3
+
+"[3]"
+
+if 'a' in 'banana':
+    print('tem')

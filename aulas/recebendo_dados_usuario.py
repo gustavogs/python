bc6d908c3d2ab776c75ae3f890b62ef2e7ce8116
gustavo <gustavo.geisleschter@gmail.com>
2026-02-15 15:43:21 -0300

    geral

---
+++ aulas/recebendo_dados_usuario.py
@@ -0,0 +1,20 @@
+"""
+Recebendo dados do usuario
+"""
+
+# Entrada de dados
+print("Qual seu nome?")
+nome = input()
+print("Qual sua idade?")
+idade = input()
+
+"""Atenção, todo dado recebido via input() é do tipo String"""
+
+# Processamento
+
+# Saida de dados
+# print('Seja bem vindo(a) %s', % nome), versão 2.x
+# print('Seja bem vindo(a) {0}', .format(nome)), versão 3.x
+print(f'Seja bem vindo(a) {nome} \nSua idade é: {idade}')  # Versão atual
+#nome = input('Qual seu nome?') versão simplificada
+#idade = input('Qual sua idade?') versão simplificada
